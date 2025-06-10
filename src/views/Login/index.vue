42c58e1eb5f1ef66f4f1deb1db3a1aa3f9c0c16b
xiaowei <2902267627@qq.com>
2025-06-10 09:14:44 +0800

    feat: 更新项目配置

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: xiaowei 2902267627@qq.com
+ * @Date: 2025-06-09 09:14:40
+ * @LastEditors: xiaowei 2902267627@qq.com
+ * @LastEditTime: 2025-06-09 15:57:40
+ * @FilePath: \car-h5-project\src\views\Login\index.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div class="login">
     <div>
@@ -12,9 +20,6 @@
 import LoginForm from './components/LoginForm.vue'
 import LoginHeader from './components/LoginHeader.vue'
 import LanguageSwitcher from '@/components/LanguageSwitcher/index.vue'
-import { useI18n } from 'vue-i18n'
-
-const { t } = useI18n()
 </script>
 
 <style lang="scss" scoped>

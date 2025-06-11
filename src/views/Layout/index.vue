bd04be73b51d58767a6840bb03c360e0b1d57991
xiaowei <2902267627@qq.com>
2025-06-11 17:08:30 +0800

    feat: 更新项目配置

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: xiaowei 2902267627@qq.com
- * @Date: 2025-06-09 09:14:40
- * @LastEditors: xiaowei 2902267627@qq.com
- * @LastEditTime: 2025-06-10 09:10:56
- * @FilePath: \car-h5-project\src\views\Layout\index.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div class="layout">
     <router-view />
--- src/views/ProfileView/index.vue
+++ src/views/ProfileView/index.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: 肖蔚 xiaowei@yw105.wecom.work
- * @Date: 2025-06-01 13:12:37
- * @LastEditors: 肖蔚 xiaowei@yw105.wecom.work
- * @LastEditTime: 2025-06-05 23:04:39
- * @FilePath: \car-h5-project\src\views\ProfileView\index.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div class="profile-view">
     <ProfileHeader />
